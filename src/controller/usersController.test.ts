9671fd0ff4d6088d2c4679a8a2ed00056ad9994c
I3run01 <brunnooa.v@gmail.com>
2023-04-25 16:45:29 -0300

    c

---
+++ src/controller/usersController.test.ts
@@ -0,0 +1,14 @@
+//import request from 'supertest';
+import app from '../../src/app';
+
+import { UsersController } from './usersController';
+import CreateUserDto from '../dto/userDTO';
+
+describe('usersController', () => {
+
+    describe('ping', () => {
+        it('should return pong', async () => {
+            //UsersController.ping()
+        })
+    })
+})
