5c9d31a0c7c371c19987c5ee3daf18d7a83373c5
Bruno Alves Vieira <brunnooa.v@gmail.com>
2023-04-25 20:25:20 -0300

    c

--- src/app.ts
+++ src/app.ts
@@ -20,19 +20,12 @@ server.use(cors({
 server.use(cookieParser())
 server.use(express.static(path.join(__dirname, '../public')))
 server.use(express.urlencoded({ extended: true}))
-server.use('/api/users' ,usersRouter)
+
+server.use('/api/users', usersRouter)
 
 server.use((req: Request, res: Response) => {
     res.status(404)
     res.json({ error: 'Endpoint not found'})
 })
-
-const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-    res.status(400)
-    console.log(err)
-    res.json({error: 'OK'})
-}
-
-server.use(errorHandler)
 
 export default server
