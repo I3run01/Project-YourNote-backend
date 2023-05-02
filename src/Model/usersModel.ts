66bdc5506846118aa04ad6cfc580f9cc36fb2986
Bruno Alves Vieira <brunnooa.v@gmail.com>
2023-05-01 22:09:55 -0300

    c

--- src/Model/usersModel.ts
+++ src/Model/usersModel.ts
@@ -5,14 +5,37 @@ type UsersType = {
     email: string
     password: string | null
     avatarImage: string | null
+    status: 'Pending' | 'Active' 
+    confirmationCode: String
 }
 
 const schema = new Schema<UsersType>({
-    name: {type: String || null, required: false},
-    email: {type: String || null, required: true},
-    password: {type: String, required: true},
-    avatarImage: {type: String || null, required: false},
-    status: {type: String, required: true}
+    name: {
+        type: String || null,
+        required: false
+    },
+    email: {
+        type: String || null,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatarImage: {
+        type: String || null,
+        required: false
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ['Pending', 'Active'], 
+        default: 'Pending'
+    },
+    confirmationCode: { 
+        type: String, 
+        unique: true 
+    },
 })
 
 const modelName: string = 'users'
