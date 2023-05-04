d8acd88e6119d0a136dbade24cac9fe579650d6e
Bruno Alves Vieira <brunnooa.v@gmail.com>
2023-05-04 09:41:28 -0300

    c

--- src/controller/usersController.ts
+++ src/controller/usersController.ts
@@ -81,9 +81,9 @@ export class UsersController {
 
                 const confirmationCode:string = confirmationEmailToken.jwtEncoded(user.id)
                  
-                utilsFn.sendConfirmationEmail(user.name, user.email, 'confirmationCode')
+                //utilsFn.sendConfirmationEmail(user.name, user.email, 'confirmationCode')
 
-                return res.status(401).send({
+                return res.status(410).send({
                     message: "Pending Account. Please Verify Your Email!, a new link was sent in your email",
                 })
             }
