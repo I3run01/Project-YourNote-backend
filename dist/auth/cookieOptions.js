3bb4988932c6e82e5524eb9d50e622c5ff925b6c
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2023-06-15 15:35:12 -0300

    chenge to obj

---
+++ dist/auth/cookieOptions.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.cookieOptions = void 0;
+exports.cookieOptions = {
+    domain: 'yournote.cloud',
+    sameSite: 'none',
+    secure: true,
+    httpOnly: true
+};
