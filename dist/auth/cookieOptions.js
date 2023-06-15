121408469c42cf96ba9b466a6422743cedee56b3
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2023-06-15 11:41:07 -0300

    c

--- dist/auth/cookieOptions.js
+++ dist/auth/cookieOptions.js
@@ -1,10 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let cookieOptions = {
-    domain: 'yournote.cloud',
-    sameSite: 'none',
-    secure: true,
-    httpOnly: true,
-};
-exports.default = cookieOptions;
+exports.cookieOptions = void 0;
 //{domain: '.yournote.cloud', sameSite: 'strict', secure: true, httpOnly: true}
